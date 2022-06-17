8bb36a9e24d3a5ba2382969263894d71412b2fa4
Steven Havko <shavko@sonicu.com>
2022-06-17 16:59:36 -0400

    Adding some python stuff

---
+++ python_intro.py
@@ -0,0 +1,21 @@
+
+# This file is meant to serve as a quick primer to Python.
+
+# The pound, or hashtag symbol is used for single line comments.
+# Python does not have a way to do multiline comments.
+
+# Python is dynamically typed.  The type is inferred.
+
+# This means variable data type can be assigned freely.
+var = 'test'
+var = 3
+
+# this allows you to do interesting operations on mixed types like.
+var * 3  # which would return 'testtesttest'
+
+# Python 3.5 introduced type hints.  But they are not enforced, really only used for documentation.
+# Good practice to use though.
+var: str = 'test'
+
+# type(var) will return the type if you need to determine a data type.
+
